2bbd4dc33c512cb93fa390686bc897ca8c038bd2
IljaNovickij97 <ilya.novickij97@gmail.com>
2017-12-05 09:49:21 -0500

    test cases

--- src/Main.java
+++ src/Main.java
@@ -27,8 +27,19 @@ public class Main {
 			sigmas = jessinter.newSigmas(initial, sigmas);
 			
 			
-			if (k > 500) {
+			if (k > 1000) {
 				System.out.println("Solution cannot be found. Please change the user requirements!");
+				double[] requirements = initial.getRequirements();
+				if (requirements[0] > initial.getR_injection()) {
+					System.out.println("Renewable Injection cannot be met.");
+				}
+				if (requirements[1] < initial.getCO2()) {
+					System.out.println("CO2 output cannot be met.");
+				}
+				if (requirements[2] < initial.getLCOE()) {
+					System.out.println("LCOE cannot be met.");
+				}
+				System.out.println(" ");
 				break;
 			}
 		}
